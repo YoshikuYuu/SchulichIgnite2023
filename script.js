f20558c54b85e645719d55e7d4c784c5847ea424
CloudyKrypton <janetfu04@gmail.com>
2023-11-20 01:22:16 -0500

    pie chart stuff

--- script.js
+++ script.js
@@ -26,87 +26,82 @@ document.addEventListener('DOMContentLoaded', function () {
         circle.style.height = originalHeight
         circle.style.left = `${(currentWidth / 610 * 106) / currentWidth * 100}%`;
         circle.style.top = `${(currentHeight / 363 * 102) / currentHeight * 100}%`;
-        console.log(currentHeight);
-        console.log(610 / currentHeight * 102);
         circle.title = `${country} - Total: ${values.total}, Cows: ${values.cows}`;
 
         circle.addEventListener('click', function () {
-            toggleZoom(circle, originalWidth, originalHeight);
+            showPopup();
+            // toggleZoom(circle, originalWidth, originalHeight);
         });
 
         visualizationContainer.appendChild(circle);
     });
 
-    function toggleZoom(circle, originalWidth, originalHeight) {
-        // Check if the circle is zoomed in
-        const isZoomedIn = circle.style.width === `${currentHeight - 20}px`;
+    function showPopup() {
+      togglePieGraph(true);
+    }
 
-        var imageContainer = document.getElementById('image-container');
-        // Get the dimensions of the image
-        var imageWidth = imageContainer.clientWidth;    
-        // Toggle between zooming in and zooming out
-        if (isZoomedIn) {
-          // Zoom out
-          circle.style.width = originalWidth;
-          circle.style.height = originalHeight;
-          circle.style.left = `${(currentWidth / 610 * 106) / currentWidth * 100}%`;
-          circle.style.top = `${(currentHeight / 363 * 102) / currentHeight * 100}%`;
+    const canvas = document.getElementById('pieGraph');
+    var context = canvas.getContext('2d');
+
+    // Set canvas dimensions based on window size
+    canvas.width = currentWidth;
+    canvas.height = currentHeight - 20;
+
+    canvas.addEventListener('click', function () {
+      togglePieGraph(false);
+      // toggleZoom(circle, originalWidth, originalHeight);
+    });
+    ////////
+
+    // Get the distance from the upper edge of the image to the top of the screen
+    const distanceToTop = image.getBoundingClientRect().top;
+
+    const canvasWidth = canvas.width;
+    const canvasHeight = canvas.height;
+  
+    const centerX = (screenWidth - canvasWidth) / 2;
+  
+      // Set the position of the canvas
+      canvas.style.left = centerX + 'px';
+      canvas.style.top = distanceToTop + 'px';
+
+      function togglePieGraph(isPieGraphVisible) {
+        if(isPieGraphVisible) {
+          drawPieGraph();
+          canvas.removeAttribute('hidden');
         } else {
-          // Zoom in
-          const rad = Math.min(currentHeight, currentWidth);
-          circle.style.width = `${rad - 20}px`;
-          circle.style.height = `${rad - 20}px`;
-          circle.style.left = `${imageWidth /2 - ((currentHeight - 20)/2)}px`;
-          circle.style.top = `5px`;
+          canvas.setAttribute('hidden', 'true')
         }
       }
 
-      ///////// pie graph
-});
-
-let isPieGraphVisible = false;
-    const pieGraphContainer2 = document.getElementById('pieGraphContainer2');
-    const canvas = document.getElementById('pieGraph');
-    const context = canvas.getContext('2d');
-
-    function togglePieGraph() {
-        isPieGraphVisible = !isPieGraphVisible;
-
-        if (isPieGraphVisible) {
-        drawPieGraph();
-        canvas.removeAttribute('hidden');
-        } else {
-        canvas.setAttribute('hidden', 'true');
-        }
-    }
-
-    function drawPieGraph() {
-        const data = [30, 60, 90]; // Example data for the pie chart
-        const centerX = canvas.width / 2;
-        const centerY = canvas.height / 2;
+      function drawPieGraph() {
+        const data = [30, 60, 90];
+        const centerX = currentWidth / 2;
+        const centerY = (currentHeight - 20) / 2;
         const radius = Math.min(centerX, centerY);
         let startAngle = 0;
 
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         data.forEach(value => {
-        const sliceAngle = (value / 90) * Math.PI;
-        context.beginPath();
-        context.moveTo(centerX, centerY);
-        context.arc(centerX, centerY, radius, startAngle, startAngle + sliceAngle);
-        context.fillStyle = getRandomColor();
-        context.fill();
-        context.closePath();
+          const sliceAngle = (value / 90) * Math.PI;
+          context.beginPath();
+          context.moveTo(centerX, centerY);
+          context.arc(centerX, centerY, radius, startAngle, startAngle + sliceAngle);
+          context.fillStyle = getRandomColor();
+          context.fill();
+          context.closePath();
 
-        startAngle += sliceAngle;
-        });
-    }
+          startAngle += sliceAngle;
+        })
+      }
 
-    function getRandomColor() {
+      function getRandomColor() {
         const letters = '0123456789ABCDEF';
         let color = '#';
         for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
-    }
+      }
+});
